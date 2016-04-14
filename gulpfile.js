c66358b3961432d76a768430c0bad9ef4ad851ad
Marylin.Meng <menglingbujie@gmail.com>
2016-04-15 00:12:32 +0800

    react use es6

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+var config = require("./config.json"),
+  gulp=require("gulp"),
+  babel = require("gulp-babel"),
+  es2015 = require("babel-preset-es2015"),
+  webpack = require("gulp-webpack"),
+  react = require("gulp-react");
+
+var PATH = config.path;
+gulp.task("default",function(){
+  // gulp.src(PATH.jsx+"/*.jsx")
+  gulp.src([PATH.js+"/a.jsx",PATH.js+"/b.jsx"])
+    .pipe(react())
+    .pipe(babel({presets:[es2015]}))
+    .pipe(gulp.dest(PATH.js))
+    .pipe(webpack({
+      output:{
+        filename:"all2.js",
+      },
+      stats:{
+        colors:true
+      }
+    }))
+    .pipe(gulp.dest(PATH.js));
+});
