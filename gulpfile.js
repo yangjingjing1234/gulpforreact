61e565df439bf029b12ba3b085124562d5025a05
menglingbujiee <menglingbujie@gmail.com>
2016-04-29 11:34:21 +0800

    1：修改js目录结构，增加dist发布目录； 2：增加图片tinypng压缩处理 3：修复watch不重新打包bug 4：增加发布字段，rev会在发布字段生成md5 hash名称

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,21 @@ var config = require("./config.json"),
 	less = require("gulp-less"),
 	uglify = require("gulp-uglify"),
 	rename = require("gulp-rename"),
-	babel = require("gulp-babel"),
 	clean = require("gulp-clean"),
 	notify = require("gulp-notify"),
-	es2015 = require("babel-preset-es2015"),
-	presetreact = require("babel-preset-react"),
 	minifycss = require("gulp-minify-css"),
 	postcss= require("gulp-postcss"),
 	px2rem = require("postcss-px2rem"),
 	plumber = require("gulp-plumber"),
-	webpack = require("gulp-webpack"),
 	changed = require("gulp-changed"),
+	tinypng = require("gulp-tinypng"),
 	rev = require("gulp-rev");
 
 var PATH = config.path;
 var _env = gulp.env;
 var _isPxToRem = _env.isflexible&&_env.isflexible=="true" || false;
+var _isPublish = _env.environment&&_env.environment=="release" || false;
+
 var selfNotify = notify.withReporter(function(options,callback){
 	options.templateOptions={
 		date:new Date()
@@ -28,7 +27,7 @@ var selfNotify = notify.withReporter(function(options,callback){
 	callback();
 });
 gulp.task("clean",function(){
-	return gulp.src([PATH.cssPath+"*.min.css",PATH.jsPath+"*.min.js"],{read:false})
+	return gulp.src([PATH.distJsPath,PATH.distCssPath],{read:false})
 	.pipe(plumber({errorHandler:notify.onError("Clean Error:<%=error.message %>")}))
 	.pipe(clean({force:true}))
 	.pipe(selfNotify({
@@ -37,55 +36,105 @@ gulp.task("clean",function(){
 	}));
 });
 
-gulp.task("reactes6:compile",function(){
-	return gulp.src(PATH.jsPath+"jsx/*.jsx")
-		.pipe(changed(PATH.jsPath))
-		.pipe(plumber({errorHandler:notify.onError("React ES6 Error:<%=error.message %>")}))
-		.pipe(babel({"presets":[presetreact,es2015]}))
-		.pipe(uglify())
+gulp.task("js:compile",function(){
+	if(_isPublish){
+		return gulp.src(PATH.jsPath+"*.js")
+		.pipe(changed(PATH.distJsPath))
+		.pipe(plumber({errorHandler:notify.onError("JS Error:<%=error.message %>")}))
 		.pipe(rename({suffix:".min"}))
-		.pipe(gulp.dest(PATH.jsPath))
-		.pipe(webpack({
-			output:{
-        path:PATH.jsPath,
-        filename:"[name].js",
-      },
-      stats: {
-          // Nice colored output
-          colors: true
-      },
-		}))
+		.pipe(uglify())
 		.pipe(rev())
-		.pipe(gulp.dest(PATH.jsPath))
-		.pipe(selfNotify({title:"React JSX (use ES6) to js and minify",message:"JSX task complete."}));
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(selfNotify({title:"JS minify",message:"JS task complete."}));
+	}else{
+		return gulp.src(PATH.jsPath+"*.js")
+		.pipe(changed(PATH.distJsPath))
+		.pipe(plumber({errorHandler:notify.onError("JS Error:<%=error.message %>")}))
+		.pipe(rename({suffix:".min"}))
+		.pipe(uglify())
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(selfNotify({title:"JS minify",message:"JS task complete."}));
+	}
+	
 });
 
 gulp.task("less:compile",function(){
-	if(_isPxToRem){
-		var processors = [px2rem({remUnit:75})];
-		return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
-			.pipe(changed(PATH.cssPath))
-			.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
-			.pipe(less())
-			.pipe(postcss(processors))
-			.pipe(gulp.dest(PATH.cssPath))
-			.pipe(rename({suffix:".min"}))
-			.pipe(minifycss())
-			.pipe(rev())
-			.pipe(gulp.dest(PATH.cssPath))
-			.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+	
+	if(_isPublish){
+		if(_isPxToRem){
+			var processors = [px2rem({remUnit:75})];
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(postcss(processors))
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(rev())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(rev.manifest())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}else{
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(rev())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(rev.manifest())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}
 	}else{
-		return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
-			.pipe(changed(PATH.cssPath))
-			.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
-			.pipe(less())
-			.pipe(gulp.dest(PATH.cssPath))
-			.pipe(rename({suffix:".min"}))
-			.pipe(minifycss())
-			.pipe(rev())
-			.pipe(gulp.dest(PATH.cssPath))
-			.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		if(_isPxToRem){
+			var processors = [px2rem({remUnit:75})];
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(postcss(processors))
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}else{
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}
 	}
+});
+var APPKEY = "DkizKeUPgUeOrOn08-sJJn7i_GrmWKRa";
+gulp.task("image::compile",function(){
+	if(_isPublish){
+		return gulp.src(PATH.imagePath+"**")
+		.pipe(changed(PATH.distImagePath))
+		.pipe(plumber({errorHandler:notify.onError("Images Error:<%=error.message %>")}))
+		.pipe(tinypng(APPKEY))
+		.pipe(rev())
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(selfNotify({title:"Image minify",message:"Image task complete."}));
+	}else{
+		return gulp.src(PATH.imagePath+"**")
+		.pipe(changed(PATH.distImagePath))
+		.pipe(plumber({errorHandler:notify.onError("Images Error:<%=error.message %>")}))
+		.pipe(tinypng(APPKEY))
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(selfNotify({title:"Image minify",message:"Image task complete."}));
+	}
+	
 });
 gulp.task("browsersync",function(){
 	browsersync.init({
@@ -105,11 +154,11 @@ gulp.task("browsersync",function(){
 	});
 });
 gulp.task("watch",function(){
-	gulp.watch(PATH.htmlPath+"*.html",browsersync.reload);
-	gulp.watch(PATH.cssPath+"less/*.less",[browsersync.reload]);
-	gulp.watch(PATH.imagePath+"*",browsersync.reload);
-	gulp.watch(PATH.jsPath+"jsx/*.jsx",[browsersync.reload]);
+	gulp.watch(PATH.htmlPath+"**/*.html",browsersync.reload);
+	gulp.watch(PATH.cssPath+"less/*.less",["less:compile",browsersync.reload]);
+	gulp.watch(PATH.imagePath+"**",["image::compile",browsersync.reload]);
+	gulp.watch(PATH.jsPath+"**/*.js",["js:compile",browsersync.reload]);
 })
 gulp.task("default",["clean"],function(){
-	gulp.start("reactes6:compile","less:compile","watch","browsersync");
+	gulp.start("js:compile","less:compile","image::compile","watch","browsersync");
 });
---
+++ gulpfile_react.js
@@ -0,0 +1,191 @@
+
+var config = require("./config.json"),
+	gulp = require("gulp"),
+	browsersync = require("browser-sync").create(),
+	less = require("gulp-less"),
+	uglify = require("gulp-uglify"),
+	rename = require("gulp-rename"),
+	babel = require("gulp-babel"),
+	clean = require("gulp-clean"),
+	notify = require("gulp-notify"),
+	es2015 = require("babel-preset-es2015"),
+	presetreact = require("babel-preset-react"),
+	minifycss = require("gulp-minify-css"),
+	postcss= require("gulp-postcss"),
+	px2rem = require("postcss-px2rem"),
+	plumber = require("gulp-plumber"),
+	webpack = require("gulp-webpack"),
+	changed = require("gulp-changed"),
+	rev = require("gulp-rev"),
+	tinypng = require("gulp-tinypng");
+
+var PATH = config.path;
+var _env = gulp.env;
+var _isPxToRem = _env.isflexible&&_env.isflexible=="true" || false;
+var _isPublish = _env.environment&&_env.environment=="release" || false;
+
+var selfNotify = notify.withReporter(function(options,callback){
+	options.templateOptions={
+		date:new Date()
+	}
+	callback();
+});
+gulp.task("clean",function(){
+	return gulp.src([PATH.distJsPath,PATH.distCssPath],{read:false})
+	.pipe(plumber({errorHandler:notify.onError("Clean Error:<%=error.message %>")}))
+	.pipe(clean({force:true}))
+	.pipe(selfNotify({
+		title:"Clean Css,Js,Image",
+		message:"Clean task complete."
+	}));
+});
+
+gulp.task("reactes6:compile",function(){
+	if(_isPublish){
+	return gulp.src(PATH.jsPath+"jsx/*.jsx")
+		.pipe(changed(PATH.jsPath))
+		.pipe(plumber({errorHandler:notify.onError("React ES6 Error:<%=error.message %>")}))
+		.pipe(babel({"presets":[presetreact,es2015]}))
+		.pipe(uglify())
+		.pipe(rename({suffix:".min"}))
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(webpack({
+			output:{
+        path:PATH.jsPath,
+        filename:"[name].js",
+      },
+      stats: {
+          // Nice colored output
+          colors: true
+      },
+		}))
+		.pipe(rev())
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(selfNotify({title:"React JSX (use ES6) to js and minify",message:"JSX task complete."}));
+      }else{
+      	return gulp.src(PATH.jsPath+"jsx/*.jsx")
+		.pipe(changed(PATH.jsPath))
+		.pipe(plumber({errorHandler:notify.onError("React ES6 Error:<%=error.message %>")}))
+		.pipe(babel({"presets":[presetreact,es2015]}))
+		.pipe(uglify())
+		.pipe(rename({suffix:".min"}))
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(webpack({
+			output:{
+        path:PATH.jsPath,
+        filename:"[name].js",
+      },
+      stats: {
+          // Nice colored output
+          colors: true
+      },
+		}))
+		.pipe(gulp.dest(PATH.distJsPath))
+		.pipe(selfNotify({title:"React JSX (use ES6) to js and minify",message:"JSX task complete."}));
+      }
+});
+
+gulp.task("less:compile",function(){
+	
+	if(_isPublish){
+		if(_isPxToRem){
+			var processors = [px2rem({remUnit:75})];
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(postcss(processors))
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(rev())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(rev.manifest())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}else{
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(rev())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(rev.manifest())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}
+	}else{
+		if(_isPxToRem){
+			var processors = [px2rem({remUnit:75})];
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(postcss(processors))
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}else{
+			return gulp.src([PATH.cssPath+"less/*.less","!"+PATH.cssPath+"less/{common,public}.less"])
+				.pipe(changed(PATH.distCssPath))
+				.pipe(plumber({errorHandler:notify.onError("Less Error:<%=error.message %>")}))
+				.pipe(less())
+				.pipe(rename({suffix:".min"}))
+				.pipe(minifycss())
+				.pipe(gulp.dest(PATH.distCssPath))
+				.pipe(selfNotify({title:"Less to Css and minify",message:"CSS task complete."}));
+		}
+	}
+});
+var APPKEY = "DkizKeUPgUeOrOn08-sJJn7i_GrmWKRa";
+gulp.task("image::compile",function(){
+	if(_isPublish){
+		return gulp.src(PATH.imagePath+"**")
+		.pipe(changed(PATH.distImagePath))
+		.pipe(plumber({errorHandler:notify.onError("Images Error:<%=error.message %>")}))
+		.pipe(tinypng(APPKEY))
+		.pipe(rev())
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(selfNotify({title:"Image minify",message:"Image task complete."}));
+	}else{
+		return gulp.src(PATH.imagePath+"**")
+		.pipe(changed(PATH.distImagePath))
+		.pipe(plumber({errorHandler:notify.onError("Images Error:<%=error.message %>")}))
+		.pipe(tinypng(APPKEY))
+		.pipe(gulp.dest(PATH.distImagePath))
+		.pipe(selfNotify({title:"Image minify",message:"Image task complete."}));
+	}
+	
+});
+gulp.task("browsersync",function(){
+	browsersync.init({
+		// ui:{
+		// 	port:80
+		// },
+		server:{baseDir:PATH.rootPath},
+		// port:80,
+		// host:"test.kaolafm.com",
+		// proxy:{
+		// 	target:"test.kaolafm.com/liveproject/liveroom",
+		// 	middleware:function(req,res,next){
+		// 		console.log(req.url);
+		// 		next();
+		// 	}
+		// }
+	});
+});
+gulp.task("watch",function(){
+	gulp.watch(PATH.htmlPath+"**/*.html",browsersync.reload);
+	gulp.watch(PATH.cssPath+"less/*.less",["less:compile",browsersync.reload]);
+	gulp.watch(PATH.imagePath+"**",["image::compile",browsersync.reload]);
+	gulp.watch(PATH.jsPath+"jsx/*.jsx",["reactes6:compile",browsersync.reload]);
+})
+gulp.task("default",["clean"],function(){
+	gulp.start("reactes6:compile","less:compile","image::compile","watch","browsersync");
+});
